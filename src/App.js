0801e2889e4869ab8df639dfffc67acfa30623f7
TBGBelt <tbgbelt@gmail.com>
2022-08-17 18:18:30 +0200

    created header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import Topmenu from './components/header/Topmenu';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Topmenu/>
+            {/*<Switch>*/}
+            {/*    <Route exact path="/">*/}
+            {/*    <Homepage/>*/}
+            {/*    </Route>*/}
+            {/*    <Route exact path="">*/}
+            {/*    <Recipepage/>*/}
+            {/*    </Route>*/}
+            {/*    <Route exact path="calculator">*/}
+            {/*    <CalculatorPage/>*/}
+            {/*    </Route>*/}
+            {/*</Switch>*/}
+            {/*<Footer/>*/}
+        </Router>
+    )
+        ;
 }
 
 export default App;
---
+++ src/components/header/Topmenu.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+// Topmenu component
+
+function Topmenu ({title, text}) {
+    return (
+        <div className="topmenu__outer-container">
+            <div className="topmenu__inner-container">
+                <h3>
+                    <NavLink to="/" exact activeClassName="homepage-logo">{title}</NavLink>
+                </h3>
+                <ul className="navigation-list">
+                    <li>
+                        <NavLink to="/" exact activeClassName="topmenu-navigation">{text}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/" exact activeClassName="topmenu-navigation">{text}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/calculator" exact activeClassName="topmenu-navigation">{text}</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Topmenu;
